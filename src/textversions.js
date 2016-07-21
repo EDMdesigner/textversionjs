6a0c3a3e6dedbf8606c5d0f0fde11976447cc7d1
stubendek <stubendek.attila@gmail.com>
2016-07-21 16:45:16 +0200

    basic empty test initialized

---
+++ src/textversions.js
@@ -0,0 +1,151 @@
+var populateChar = function(ch, amount){
+	var result = "";
+	for(var i=0; i<amount; i += 1){
+		result += ch;
+	}
+	return result;
+};
+
+module.exports = function(htmlText, styleConfig) {
+
+	// define default styleConfig
+	var linkProcess = null;
+	var imgProcess = null;
+	var headingStyle = "plainify"; // hashify, underline
+	var listStyle = "linebreak"; // indention
+	var uIndentionChar = "-";
+	var listIndentionTabs = 3;
+	var oIndentionChar = "-";
+
+	// or accept user defined config
+	if(!!styleConfig){
+		if(typeof styleConfig.linkProcess === "function") {
+			linkProcess = styleConfig.linkProcess;
+		}
+		if(typeof styleConfig.imgProcess === "function") {
+			imgProcess = styleConfig.imgProcess;
+		}
+		if(!!styleConfig.headingStyle) {
+			headingStyle = styleConfig.headingStyle;
+		}
+		if(!!styleConfig.listStyle) {
+			listStyle = styleConfig.listStyle;
+		}
+		if(!!styleConfig.uIndentionChar) {
+			uIndentionChar = styleConfig.uIndentionChar;
+		}
+		if(!!styleConfig.listIndentionTabs) {
+			listIndentionTabs = styleConfig.listIndentionTabs;
+		}
+		if(!!styleConfig.oIndentionChar) {
+			oIndentionChar = styleConfig.oIndentionChar;
+		}
+	}
+
+	var uIndention = populateChar(uIndentionChar, listIndentionTabs);
+
+	// removel all \n linebreaks
+	var tmp = String(htmlText).replace(/\n|\r/g, " ");
+
+	// remove everything before and after <body> tags including the tag itself
+	tmp = tmp.replace(/<\/body>.*/i, "");
+	tmp = tmp.replace(/.*<body[^>]*>/i, "");
+
+	// remove or replace images - replacement texts with <> tags will be removed also, if not intentional, try to use other notation
+	tmp = tmp.replace(/<img([^>]*)>/gi, function(str, imAttrs) {
+		var imSrc = "";
+		var imAlt = "";
+		var imSrcResult = (/src="([^"]*)"/i).exec(imAttrs);
+		var imAltResult = (/alt="([^"]*)"/i).exec(imAttrs);
+		if(imSrcResult !== null){
+			imSrc = imSrcResult[1];
+		}
+		if(imAltResult !== null){
+			imAlt = imAltResult[1];
+		}
+		if(typeof(imgProcess) === "function") {
+			return imgProcess(imSrc, imAlt);
+		}
+		return "\n" + (imAlt)+" ["+ imSrc + "]\n";
+	});
+
+
+	function createListReplaceCb() {
+		return function(match, listType, listAttributes, listBody) {
+			var liIndex = 0;
+			if(listAttributes && /start="([0-9])+"/i.test(listAttributes)) {
+				liIndex = (/start="([0-9])"/i.exec(listAttributes)[1])-1;
+			}
+			var plainListItem = "<p>" + listBody.replace(/<li[^>]*>(((?!<li[^>]*>)(?!<\/li>).)*)<\/li>/gi, function(str, listItem) {
+				var actSubIndex = 0;
+				var plainListLine = listItem.replace(/(^|(<br \/>))(?!<p>)/gi, function(){
+					if(listType === "o" && actSubIndex === 0){
+						liIndex += 1;
+						actSubIndex += 1;
+						return "<br />" + liIndex + populateChar(oIndentionChar, listIndentionTabs-(String(liIndex).length));
+					}
+					return "<br />" + uIndention;
+				});
+				return plainListLine;
+			})+"</p>";
+			return plainListItem;
+		};
+	}
+
+	// handle lists
+	if(listStyle === "linebreak"){
+		tmp = tmp.replace(/<\/?ul[^>]*>|<\/?ol[^>]*>|<\/?li[^>]*>/gi, "\n");
+	}
+	else if(listStyle === "indention"){
+		while( /<(o|u)l[^>]*>(.*)<\/\1l>/gi.test(tmp)){
+			tmp = tmp.replace(/<(o|u)l([^>]*)>(((?!<(o|u)l[^>]*>)(?!<\/(o|u)l>).)*)<\/\1l>/gi, createListReplaceCb());
+		}
+	}
+
+	// handle headings
+	if(headingStyle === "underline") {
+		tmp = tmp.replace(/<h1[^>]*>([^<]*)<\/h1>/gi, function(str, p1) {
+			return p1 + "\n" + populateChar("=", p1.length) + "\n";
+		});
+		tmp = tmp.replace(/<h2[^>]*>([^<]*)<\/h2>/gi, function(str, p1) {
+			return p1 + "\n" + populateChar("-", p1.length) + "\n";
+		});
+		tmp = tmp.replace(/<h([3-6])[^>]*>([^<]*)<\/h\1>/gi, function(str, p1, p2) {
+			return "\n" + p2 + "\n";
+		});
+	}
+	else if(headingStyle === "hashify") {
+		tmp = tmp.replace(/<h([1-6])[^>]*>([^<]*)<\/h\1>/gi, function(str, p1, p2) {
+			return populateChar("#", p1) + " " + p2 + "\n";
+		});
+	}
+
+	// replace <br>s, <tr>s, <divs> and <p>s with linebreaks
+	tmp = tmp.replace(/<br[^>]*>|<p[^>]*>|<\/p[^>]*>|<div[^>]*>|<\/div[^>]*>|<tr[^>]*>|<\/tr[^>]*>/gi, "\n");
+
+	// remove all tags except links
+	tmp = tmp.replace(/<[^a\/][^>]*>/gi, "");
+	tmp = tmp.replace(/<\/[^a][^>]*>/gi, "");
+
+	// replace <a href>b<a> links with b (href) or as described in the linkProcess function
+	tmp = tmp.replace(/<a[^>]*href="([^"]*)"[^>]*>([^<]+)<\/a[^>]*>/gi, function(str, href, linkText) {
+		if(typeof linkProcess === "function") {
+			return linkProcess(href, linkText);
+		}
+		return linkText+" ["+ href + "]";
+	});
+
+	// remove empty lines
+	tmp = tmp.replace(/\n[ \t]*/gi, "\n");
+
+	// remove duplicated newlines
+	tmp = tmp.replace(/\n\n+/gi, "\n");
+
+	// remove duplicated spaces including non braking spaces
+	tmp = tmp.replace(/( |&nbsp;|\t)+/gi, " ");
+
+	// remove line starter spaces
+	tmp = tmp.replace(/^ +/gi, "");
+
+	return tmp;
+};
